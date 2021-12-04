bd2d1243e9eb184604cbd7fc831dd9635e9fcc10
Tiago Sabadini <sabadini.tiago@gmail.com>
2021-12-04 08:45:18 -0300

    Adiciona projeto com Eclipse IDE

---
+++ ide-java/eclipse-projeto-1/eclipse-projeto-1/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato("Marlo", "Marrom", 3);
+		System.out.println(gato);
+
+	}
+
+}
